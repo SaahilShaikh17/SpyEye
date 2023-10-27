3f3681c7699b1fba54b2c79f6ae9824979f1eb95
Saahil Shaikh <saahils191@gmail.com>
2023-10-28 02:11:20 +0530

    updated code

--- SpyEye.py
+++ SpyEye.py
@@ -11,20 +11,21 @@ import tensorflow_datasets as tfds
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # LSTM 
-max_words = 1000
 max_len = 150
-
 
 # Load the machine learning models
 model = joblib.load('naive_bayes_classifier.pkl')
-model_1= joblib.load('random_forest_cl.pkl')
+model_1 = joblib.load('random_forest_cl.pkl')
 model_2 = tf.keras.models.load_model('LSTM_retrained.h5')
-
 
 # Load the vectorizers
 vectorizer = joblib.load('vectorizer.pkl')
-vectorizer_1= joblib.load('vectorizer_random.pkl')
-tok=joblib.load('tokenizer_retrained.pickle')
+vectorizer_1 = joblib.load('vectorizer_random.pkl')
+tok = joblib.load('tokenizer_retrained.pickle')
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
 
 # Defining the lemma function
 def lemma(test_pred):
@@ -45,7 +46,7 @@ def lemma(test_pred):
 
 # Defining the Streamlit application
 def main():
-    st.title('SpyEye-An Email Monitoring System to detect Spam')
+    st.title('SpyEye-An Email Monitoring System to Detect Spam')
     st.write('Enter some input data to get a prediction:')
     input_email = st.text_area('Input data:')
 
@@ -60,60 +61,40 @@ def main():
 
         # Transform the input email using the vectorizer
         test_data_transformed = vectorizer.transform(test_pred)
-        test_data_transformed_1= vectorizer_1.transform(test_pred)
+        test_data_transformed_1 = vectorizer_1.transform(test_pred)
 
-        #create dataframe
-        list=[{'Classification':None,'Spam Probability':None,
-              'Ham Probability':None},{'Classification':None,'Spam Probability':None,'Ham Probability':None},
-             {'Classification':None,'Spam Probability':None,'Ham Probability':None}]
-        data=pd.DataFrame(list,columns=['Classification','Spam Probability','Ham Probability'],index=['Random Forest','Naive Bayes','LSTM'])
-        
-        #Prediction
-        if st.button('Predict'):
-            pred_proba = model.predict_proba(test_data_transformed)[0]
-            pred_label = model.predict(test_data_transformed)[0]
+        # Create dataframe to store results
+        results = {
+            'Classifier': ['Naive Bayes', 'Random Forest', 'LSTM'],
+            'Classification': [None, None, None],
+            'Spam Probability': [None, None, None],
+            'Ham Probability': [None, None, None]
+        }
 
-            pred_proba_1 = model_1.predict_proba(test_data_transformed)[0]
-            pred_label_1 = model_1.predict(test_data_transformed)[0]
+        # Make predictions
+        pred_proba = model.predict_proba(test_data_transformed)[0]
+        pred_label = model.predict(test_data_transformed)[0]
 
-            txts = tok.texts_to_sequences(test_pred)
-            txts = pad_sequences(txts, maxlen=max_len)
-            prediction = model_2.predict(txts)[0][0]
-        
-            #Naive Bayes Classifier
-            if pred_label == 'ham':
-                data.at['Naive Bayes','Classification']=pred_label
-                data.at['Naive Bayes','Spam Probability']=pred_proba[1].round(3)
-                data.at['Naive Bayes','Ham Probability']=pred_proba[0].round(3)
-            else:
-                data.at['Naive Bayes','Classification']=pred_label
-                data.at['Naive Bayes','Spam Probability']=pred_proba[1].round(3)
-                data.at['Naive Bayes','Ham Probability']=pred_proba[0].round(3)
-            
+        pred_proba_1 = model_1.predict_proba(test_data_transformed)[0]
+        pred_label_1 = model_1.predict(test_data_transformed)[0]
 
-            #Random Forest Classifier
-            if pred_label_1 == 'ham':
-                data.at['Random Forest','Classification']=pred_label_1
-                data.at['Random Forest','Spam Probability']=pred_proba_1[1].round(3)
-                data.at['Random Forest','Ham Probability']=pred_proba_1[0].round(3)
-            else:
-                data.at['Random Forest','Classification']=pred_label_1
-                data.at['Random Forest','Spam Probability']=pred_proba_1[1].round(3)
-                data.at['Random Forest','Ham Probability']=pred_proba_1[0].round(3)
+        txts = tok.texts_to_sequences(test_pred)
+        txts = pad_sequences(txts, maxlen=max_len)
+        prediction = model_2.predict(txts)[0][0]
 
-            #LSTM
-            if (prediction > 0.5):
-                data.at['LSTM','Classification']='spam'
-                data.at['LSTM','Spam Probability']=prediction.round(3)
-                data.at['LSTM','Ham Probability']=1-prediction.round(3)
-            else:
-                data.at['LSTM','Classification']='ham'
-                data.at['LSTM','Spam Probability']=prediction.round(3)
-                data.at['LSTM','Ham Probability']=1-prediction.round(3)
-        
-            st.table(data)
+        results['Classification'][0] = pred_label
+        results['Spam Probability'][0] = pred_proba[1].round(3)
+        results['Ham Probability'][0] = pred_proba[0].round(3)
 
-              
+        results['Classification'][1] = pred_label_1
+        results['Spam Probability'][1] = pred_proba_1[1].round(3)
+        results['Ham Probability'][1] = pred_proba_1[0].round(3)
+
+        results['Classification'][2] = 'spam' if prediction > 0.5 else 'ham'
+        results['Spam Probability'][2] = prediction.round(3)
+        results['Ham Probability'][2] = (1 - prediction).round(3)
+
+        st.table(pd.DataFrame(results))
 
 if __name__ == '__main__':
     main()
